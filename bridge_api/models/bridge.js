1c42631d4a0adb64798884f51daef956475c2904
Zach Doll <zach.doll@gmail.com>
2020-07-22 15:04:24 -0500

    updated schema for user and bridge

--- bridge_api/models/bridge.js
+++ bridge_api/models/bridge.js
@@ -1,8 +1,19 @@
 let mongoose = require('mongoose')
 
 let bridgeSchema = mongoose.Schema({
-    
-  
+    firstName: {type: String},
+    lastName: {type: String},
+    bio: {type: String},
+    status: {type: String},
+    img: {type: String},
+    friendsRequests: {type: Array},
+    spotifyList: {type: Object},
+    posts: {type: Object},
+    inbox: {type: Object},
+    friendList: {type: Array},
+    youtubeList: {type: Object},
+
 })
+
 
 module.exports = mongoose.model('Bridge', bridgeSchema)
