cf5e41934adad5d498c33b9c1187b6224efaafe3
Alireza Nasiri <alirezanasiri@Alirezas-MBP.lan>
2020-07-22 15:22:23 -0500

    updated

--- bridge_front_end/bridge/src/components/Login.jsx
+++ bridge_front_end/bridge/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+// import logo from './public/logo.png'
 export default class Login extends Component {
     render() {
         return (
@@ -12,14 +12,14 @@ export default class Login extends Component {
                         <input type='submit' value='Log-In'></input>
                     </form> 
                     </div>
-                    <img className='logo' src='./public/logo.png' />
+                    <img className='logo'  />
                     <div>
                         <form className='sign-up'>
-                        <input type='text' value="firstname" name='firstname'></input>
-                        <input type='text' value="lastname" name='lastname'></input>
-                        <input type='email' value="email" name='email'></input>
-                        <input type='password' value="password" name='password'></input>
-                        <input type='submit' value='Sign-Up'></input>
+                            <input type='text' value="firstname" name='firstname'></input>
+                            <input type='text' value="lastname" name='lastname'></input>
+                            <input type='email' value="email" name='email'></input>
+                            <input type='password' value="password" name='password'></input>
+                            <input type='submit' value='Sign-Up'></input>
                         </form> 
                     </div>                    
                 </div>
