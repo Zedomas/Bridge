381dbe7fa0a4c8e59c59a9be1546f187b9dd7685
Zach Doll <zach.doll@gmail.com>
2020-07-23 17:08:53 -0500

    added music controller and model page and installed env variables with env folder and updated database connection with variables

---
+++ bridge_api/Controller/music.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const music = express.Router()
+const Songs = require('../models/music.js')
+
+// // SHOW ROUTE for new users //
+// movie.post('/login', (req, res) => {
+
+//   console.log(req.body)
+  
+//   User.findOne({ username: req.body.username}, (err, foundUser) => {
+//     console.log(foundUser)
+//     if (!foundUser) {
+//         res.status(400).json({error: message})
+//         // need to send back a no found user
+//     } else {
+//         if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+//             res.status(200).json(foundUser) 
+//         } else {
+//            //needs to say failed password match
+//         }
+//     }
+//   })
+
+// })
+
+
+// Add a movie
+music.post('/add', async (req, res) => {
+  console.log(req.body)
+  Songs.create(req.body, (err, createdSong) => {
+    if (err) {
+        res.status(400).json({error: error.message})
+    }
+        res.status(200).send(createdSong)
+  })
+})
+
+module.exports = music
