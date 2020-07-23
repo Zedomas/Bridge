fe7f4d1f8e0627cb5dcef969a2f1beaf0d2c8f18
Zedomas <Zedomas@gmail.com>
2020-07-23 09:54:34 -0500

    added login function

--- bridge_api/Controller/users_controller.js
+++ bridge_api/Controller/users_controller.js
@@ -4,13 +4,24 @@ const users = express.Router()
 const User = require('../models/user.js')
 
 // SHOW ROUTE for new users //
-users.get('/login', (req, res) => {
-  User.find({}, (err, foundUser) => {
-      if (err) {
-          res.status(400).json({error: message})
-      }
-      res.status(200).json(foundUser)
+users.post('/login', (req, res) => {
+
+  console.log(req.body)
+  
+  User.findOne({ username: req.body.username}, (err, foundUser) => {
+    console.log(foundUser)
+    if (!foundUser) {
+        res.status(400).json({error: message})
+        // need to send back a no found user
+    } else {
+        if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+            res.status(200).json(foundUser) 
+        } else {
+           //needs to say failed password match
+        }
+    }
   })
+
 })
 
 
