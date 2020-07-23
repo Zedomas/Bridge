6d933b9bfcd506371e77d419a385eb56f6068766
Zedomas <Zedomas@gmail.com>
2020-07-23 13:37:12 -0500

    Fixed login to add to main state

--- bridge_front_end/bridge/src/components/Login.jsx
+++ bridge_front_end/bridge/src/components/Login.jsx
@@ -5,23 +5,9 @@ import logo from '../img/logo.png'
 
 export default class Login extends Component {
 
-    state = {
-        LoginUsername: '',
-        LoginPassword: '',
-        NewUsername: '',
-        NewPassword: '',
-        email: '',
-    }        
+        
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id]: event.target.value,
-        });
-    }
-
-
-
-
+    
     render() {
         return (
             <div>
@@ -29,9 +15,8 @@ export default class Login extends Component {
                     <div className='log-in'>
 
                     <form onSubmit={ (evt) => this.props.handleLogin(evt) } >
-                        <input type='text' value={this.state.LoginUsername} id='LoginUsername' placeholder='Username' onChange={this.handleChange} ></input>
-
-                        <input type='password' value={this.state.LoginPassword} id='LoginPassword' placeholder='Password' onChange={this.handleChange} ></input>
+                        <input type='text' value={this.props.LoginUsername} id='LoginUsername' placeholder='Username' onChange={this.props.handleChange} ></input>
+                        <input type='password' value={this.props.LoginPassword} id='LoginPassword' placeholder='Password' onChange={this.props.handleChange} ></input>
                         <input type='submit' value='Log-In' ></input>
                     </form> 
 
@@ -41,10 +26,9 @@ export default class Login extends Component {
                     </div>
                     <div>
                         <form className='sign-up' onSubmit={ (evt) => this.props.handleSignUp(evt) } >
-
-                        <input type='text' value={this.state.NewUsername} id='NewUsername' placeholder='Username' onChange={this.handleChange} ></input>
-                        <input type='email' placeholder='Email' onChange={this.handleChange} value={this.state.email} id='email'></input>
-                        <input type='password' placeholder='Password' onChange={this.handleChange} value={this.state.NewPassword} id='NewPassword'></input>
+                        <input type='text' value={this.props.NewUsername} id='NewUsername' placeholder='Username' onChange={this.props.handleChange} ></input>
+                        <input type='email' placeholder='Email' onChange={this.props.handleChange} value={this.props.email} id='email'></input>
+                        <input type='password' placeholder='Password' onChange={this.props.handleChange} value={this.props.NewPassword} id='NewPassword'></input>
                         <input type='submit' value='Sign-Up'></input>
 
                         </form> 
