556f24f4ab3c1c6b85eb206331ead8af0963eeb5
Zedomas <Zedomas@gmail.com>
2020-07-24 13:40:19 -0500

    add user state pass down

--- bridge_front_end/bridge/src/App.js
+++ bridge_front_end/bridge/src/App.js
@@ -127,8 +127,13 @@ render () {
             email = {this.state.email}
             />
         } />
-          <Route exact path='/movies' render={()=><Movie username={this.state.username}/>}/>
-          <Route exact path='/music' component={Music}/>
+          <Route exact path='/movies' render={()=><Movie 
+          user={this.state.username}
+
+          />}/>
+          <Route exact path='/music' render={() => <Music 
+          user={this.state.username}
+          />}/>
         </Switch>
       
       </div>
