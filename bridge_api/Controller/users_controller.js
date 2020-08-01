3354d53f666db8d22dda009c798b92964a2bd094
Alireza Nasiri <alirezanasiri@Alirezas-MBP.lan>
2020-08-01 13:55:28 -0500

    add session and jsonwebtoken for login

--- bridge_api/Controller/users_controller.js
+++ bridge_api/Controller/users_controller.js
@@ -2,40 +2,52 @@ const bcrypt = require('bcrypt')
 const express = require('express')
 const users = express.Router()
 const User = require('../models/user.js')
+const jwt = require('jsonwebtoken');
 
-// SHOW ROUTE for new users //
+
+// LOGIN ROUTE
 users.post('/login', (req, res) => {
+  console.log(req.body);
+    User.findOne({username:req.body.username}, (err, foundUser) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      }
+      console.log("success")
+      console.log(req.body.password)
+      console.log(foundUser.password)
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        console.log(process.env.SECRET)
+        let token = jwt.sign(
+          { userId: foundUser.id, username: foundUser.username},
+          'letsgo',
+          { expiresIn: '1h' });
+          console.log(foundUser);
+        res.status(200).json({
+          username: foundUser.username,
+          password:foundUser.password,
+          token: token,
+        });
 
-  console.log(req.body)
-  
-  User.findOne({ username: req.body.username}, (err, foundUser) => {
-    console.log(foundUser)
-    if (!foundUser) {
-        res.status(400).json({error: message})
-        // need to send back a no found user
     } else {
-        if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-            res.status(200).json(foundUser) 
-        } else {
-           //needs to say failed password match
-        }
+        res.status(401).json({message:"Invalid Username/Password"});
     }
+     
+    })
   })
 
-})
-
-
-// Sign up route
-users.post('/signup', async (req, res) => {
-  //overwrite the user password with the hashed password, then pass that in to our database
-  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-  console.log(req.body)
-  User.create(req.body, (err, createdUser) => {
-    if (err) {
-        res.status(400).json({error: error.message})
-    }
-        res.status(200).send(createdUser)
+  // CREATE ROUTE
+users.post('/signup', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    User.create(req.body, (error, createdUser) => {
+      if (error) {
+        res.status(400).json({ error: error.message })
+      }
+      res.status(200).send(createdUser)
+      console.log(createdUser)
+    })
   })
-})
+
+
+
 
 module.exports = users
--- bridge_api/models/user.js
+++ bridge_api/models/user.js
@@ -3,7 +3,6 @@ let mongoose = require('mongoose')
 let userSchema = new mongoose.Schema({
     username: {type:String, unique: true, required: true},
     password: {type:String, required: true},
-    email: {type: String, required: true}
 })
 
 
